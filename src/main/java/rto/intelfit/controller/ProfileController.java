f9a948abebced81dbef2963212e0babfd0a117d7
Your Name <you@example.com>
2025-10-22 18:28:37 +0900

    c

--- src/main/java/rto/intelfit/controller/ProfileController.java
+++ src/main/java/rto/intelfit/controller/ProfileController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+//impoer
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
