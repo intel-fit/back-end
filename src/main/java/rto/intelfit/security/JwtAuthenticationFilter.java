b98470fb89b7f0f79610fb1a70e41f9ef7a5a891
Lee Won Woong <dldnjsdnddl@naver.com>
2025-11-30 19:08:58 +0900

    fix: kakao unlink + redirect-uri + profile delete 정리

--- src/main/java/rto/intelfit/security/JwtAuthenticationFilter.java
+++ src/main/java/rto/intelfit/security/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             "/api/users/kakao/webview-login", //이원웅 추가
             "/api/users/kakao/callback", //이원웅 추가
             "/api/users/kakao/logout", //이원웅 추가
-            "/api/users/kakao/logout/callback" //이원웅 추가
+            "/api/users/kakao/logout/callback", //이원웅 추가
             "/api/payments/stripe/webhook"
     );
 
