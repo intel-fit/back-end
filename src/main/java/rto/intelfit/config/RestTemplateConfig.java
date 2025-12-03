85771be14de9524e1a90a49807cc09abda1ab0d1
Your Name <you@example.com>
2025-12-03 17:54:17 +0900

    타임아웃 변경

--- src/main/java/rto/intelfit/config/RestTemplateConfig.java
+++ src/main/java/rto/intelfit/config/RestTemplateConfig.java
@@ -2,7 +2,6 @@ package rto.intelfit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,14 +9,6 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        // 타임아웃 설정
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000); // 5초
-        factory.setConnectTimeout(30000);   // 30초
-        restTemplate.setRequestFactory(factory);
-
-        return restTemplate;
+        return new RestTemplate();
     }
 }
