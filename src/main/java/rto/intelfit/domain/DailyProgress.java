1e17e106562bdebb0edf382eac03029bb973636d
use030 <use030@naver.com>
2025-12-17 11:07:18 +0900

    시연 계정용 볼륨 초기화

--- src/main/java/rto/intelfit/domain/DailyProgress.java
+++ src/main/java/rto/intelfit/domain/DailyProgress.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 public class DailyProgress {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //a
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
